25faf3631626223eae9cc5a82c643110b000b607
bl00na <marc.humpert@gmx.net>
2020-06-12 10:50:42 +0200

    Updated files and added comments

--- api/keys.js
+++ api/keys.js
@@ -1,9 +1,11 @@
+// Here we map the Kubernetes container environment variables defined in the Pod/Deployment
+// definition file to JavaScript variables to use them in our code, f.e.: keys.redisHost
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT
+  pgUser: process.env.POSTGRES_USER,
+  pgHost: process.env.POSTGRES_HOST,
+  pgPort: process.env.POSTGRES_PORT,
+  pgDatabase: process.env.POSTGRES_DATABASE,
+  pgPassword: process.env.POSTGRES_PASSWORD
 };
--- worker/keys.js
+++ worker/keys.js
@@ -1,3 +1,5 @@
+// Here we map the Kubernetes container environment variables defined in the Pod/Deployment
+// definition file to JavaScript variables to use them in our code, f.e.: keys.redisHost
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT
